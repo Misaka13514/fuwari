96bd50c2dd684c963d49e0595677381b1f4ecfcf
Rine Amakawa <Misaka13514@gmail.com>
2026-08-26 10:25:28 +0900

    [U] Update astro: 5.13.10 -> 7.2.7

---
+++ src/content.config.ts
@@ -0,0 +1,30 @@
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
+
+export const collections = {
+	posts: defineCollection({
+		loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/posts" }),
+		schema: z.object({
+			title: z.string(),
+			published: z.date(),
+			updated: z.date().optional(),
+			draft: z.boolean().optional().default(false),
+			description: z.string().optional().default(""),
+			image: z.string().optional().default(""),
+			tags: z.array(z.string()).optional().default([]),
+			category: z.string().optional().nullable().default(""),
+			lang: z.string().optional().default(""),
+
+			/* For internal use */
+			prevTitle: z.string().default(""),
+			prevSlug: z.string().default(""),
+			nextTitle: z.string().default(""),
+			nextSlug: z.string().default(""),
+		}),
+	}),
+	spec: defineCollection({
+		loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/spec" }),
+		schema: z.object({}),
+	}),
+};
